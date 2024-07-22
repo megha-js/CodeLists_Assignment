6168b5975c08478f105470625409b6392fddee72
megha-js <130333862+megha-js@users.noreply.github.com>
2024-07-22 11:40:14 +0530

    Update server.js

--- CodeLists_Assignment/backend/server.js
+++ CodeLists_Assignment/backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+
 const app = express();
 const port = 5000;
 
@@ -21,5 +22,6 @@ const listRoutes = require('./routes/listRoutes.js');
 app.use('/api/users', userRoutes);
 app.use('/api/lists', listRoutes);
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => 
+  console.log(`Server running on port ${port}`));
  
